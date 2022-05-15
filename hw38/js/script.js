d4de70d1985562489c3a633f35889e61a7c537f7
dronovs <od270891dss@gmail.com>
2022-05-15 21:51:46 +0300

    update

--- hw38/js/script.js
+++ hw38/js/script.js
@@ -1,35 +1,22 @@
 'use strict';
 
-const anyArray = [1, 2, 3, [4, 5, 6, [7]]];
+const anyArray = [1, 2, 3, [4, 5, 6, [7]], 8, 9];
 let mainUl = document.createElement('ul');
 document.body.append(mainUl);
 
-
-function  generateList(arr) {
-    let innerUl = document.createElement('ul');
+function generateList (arr, ul) {
     for (let item of arr) {
+        const li = document.createElement('li');
         if (Array.isArray(item)) {
-            for (let elem of item) {
-                mainUl.append(innerUl);
-                let innerLi = document.createElement('li');
-                innerLi.innerText = elem;
-                innerUl.append(innerLi);
-                if (Array.isArray(elem)) {
-                    let subUl = document.createElement('ul');
-                    for (let element of elem) {
-                            innerUl.append(subUl);
-                            let subLi = document.createElement('li');
-                            subLi.innerText = element;
-                            subUl.append(subLi);
-                    }
-                }
-            }
+            const innerUl = document.createElement('ul');
+            li.append(innerUl);
+            generateList(item, innerUl);
         } else {
-            let mainInnerLi = document.createElement('li');
-            mainInnerLi.innerText = item;
-            mainUl.append(mainInnerLi);
+            li.innerText = item;
         }
+        ul.append(li);
     }
 }
 
-generateList(anyArray);
+generateList(anyArray, mainUl);
+
